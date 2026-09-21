c298c4000b3278d35519d42e924f6a9e67e9eaac
Levy Moraes <moraeslevy78@gmail.com>
2026-09-21 01:04:45 +0000

    projeto concluido

---
+++ index.js
@@ -0,0 +1,49 @@
+// Dados do Herói
+let heroiNome = "Finn"
+let heroiHp = 50
+let heroiAtaque = 6
+
+// Dados do Monstro
+let monstroNome = "Hunson Abadeer"
+let monstroHp = 25
+let monstroDefesa = 10
+let monstroNivel = "Chefe"
+
+console.log(` Batalha Iniciada: ${heroiNome} VS ${monstroNome} `)
+
+// 1. CÁLCULO DO ATAQUE DO HERÓI
+let danoCausado = heroiAtaque - monstroDefesa
+
+if (danoCausado <= 0) {
+    console.log(` O ${monstroNome} defendeu o golpe! Nenhum dano causado.`)
+} else {
+    monstroHp = monstroHp - danoCausado
+    console.log(` ${heroiNome} atacou com sucesso! Causou ${danoCausado} de dano.`)
+}
+
+// 2. VERIFICAÇÃO SE O MONSTRO MORREU
+if (monstroHp <= 0) {
+    console.log(` O ${monstroNome} foi derrotado!`)
+    
+    // 3. DISTRIBUIÇÃO DE RECOMPENSAS (IF ANINHADO)
+    console.log(" Calculando Recompensas...")
+    if (monstroNivel === "Comum") {
+        console.log(" Recompensa: 10 Moedas de Ouro.")
+    } else if (monstroNivel === "Elite") {
+        console.log(" Recompensa: 50 Moedas de Ouro + Poção de Vida.")
+    } else if (monstroNivel === "Chefe") {
+        console.log(" RECOMPENSA LENDÁRIA: 200 Moedas + Espada de sangue de demonio! ")
+    }
+
+} else {
+    // 4. CONTRA-ATAQUE DO MONSTRO (Se ele sobreviveu)
+    console.log(` O ${monstroNome} sobreviveu com ${monstroHp} de HP e contra-atacou!`)
+    let danoMonstro = 15
+    heroiHp = heroiHp - danoMonstro
+    
+    if (heroiHp <= 0) {
+        console.log(` Fim de Jogo! ${heroiNome} caiu em combate.`)
+    } else {
+        console.log(` ${heroiNome} resistiu ao golpe! HP restante: ${heroiHp}`)
+    }
+}
